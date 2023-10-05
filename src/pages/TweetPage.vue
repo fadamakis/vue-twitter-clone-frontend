25560d72a5439977eb3dab16a0eceaab39c04e36
Fotis Adamakis <fadamakis@gmail.com>
2023-10-05 22:24:52 +0200

    tweet page

--- src/pages/TweetPage.vue
+++ src/pages/TweetPage.vue
@@ -6,12 +6,22 @@ import { SearchWidget } from "@/features/search";
 import { TweetSingle } from "@/features/tweets";
 import { TweetReplyForm } from "@/features/tweet-create";
 import { TweetList } from "@/features/tweets";
+
+const tweet = {
+  user: {
+    name: "Fotis Adamakis",
+    handle: "@fadamakis",
+    image: "https://pbs.twimg.com/profile_images/1263362878922469376/KdZALDFP_normal.jpg",
+  },
+  body: "Hello World!",
+  date: "16 April",
+};
 </script>
 
 <template>
   <ThreeColumnLayout>
     <PageTitle has-back-button>Post</PageTitle>
-    <TweetSingle />
+    <TweetSingle :tweet="tweet" />
     <TweetReplyForm />
     <TweetList />
     <template #sidebar>
